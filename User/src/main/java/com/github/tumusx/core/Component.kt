ea682e9242c311900a5873c5c6104619519c47ee
Murillo Alves da Silva <murillosilva@MMB031757CPS.local>
2025-01-30 13:48:13 -0300

    chabnged modifiers

--- User/src/main/java/com/github/tumusx/core/Component.kt
+++ User/src/main/java/com/github/tumusx/core/Component.kt
@@ -1,12 +1,20 @@
 package com.github.tumusx.core
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Component() {
-    Column {
-        Text("Módulo de usuário!")
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text("Módulo de usuário!", Modifier.padding(16.dp))
+        Text(
+            "Este é um módulo de usuário que pode ser utilizado em qualquer aplicação.",
+            Modifier.padding(16.dp)
+        )
     }
 }
